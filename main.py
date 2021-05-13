cec028c485566ff2beea68abffae371b7309a4cf
Sanskar <sethsanskar@gmail.com>
2021-05-13 11:47:22 +0530

    updated code

--- main.py
+++ main.py
@@ -2,46 +2,94 @@ import datetime
 import time
 from bs4 import BeautifulSoup
 import requests
-from plyer import notification
+from win10toast import ToastNotifier
+from datetime import datetime as dt
+import os
+
+toast = ToastNotifier()
 
 
-html_text = requests.get('https://www.cricbuzz.com/').text
+def resource_path(relative_path):
+    try:
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
 
-soup = BeautifulSoup(html_text, 'lxml')
+    return os.path.join(base_path, relative_path)
 
-matches = soup.find_all(
-    'li', class_='cb-col cb-col-25 cb-mtch-blk cb-vid-sml-card-api videos-carousal-item cb-carousal-item-large cb-view-all-ga')
 
-for match in matches:
-    match_name = match.find('a')
-    result = match_name.find('div', class_='cb-ovr-flo cb-text-complete')
-    # toss = match_name.find('div', class_='cb-ovr-flo cb-text-preview')
+# toast.show_toast("CricPing", "Software was successfully installed..." + '\n' + "RESTART your PC now",
+#                  duration=15, icon_path=resource_path("bell.ico"))
 
-    # try:
-    #     if toss.text:
-    #         print(f"Match: {match_name['title']}")
-    #         print(f"Toss: {toss}")
-    #         print('')
-    # except:
-    #     continue
 
-    if result:
+def cric():
+
+    now = dt.now()
+    current_time = now.strftime("%H:%M:%S")
+    # print(current_time)
+
+    cbuzz = requests.get('https://www.cricbuzz.com/').text
+
+    soup = BeautifulSoup(cbuzz, 'lxml')
+
+    matches = soup.find_all(
+        'li', class_='cb-col cb-col-25 cb-mtch-blk cb-vid-sml-card-api videos-carousal-item cb-carousal-item-large cb-view-all-ga')
+
+    for match in matches:
+        match_name = match.find('a')
+        result = match_name.find('div', class_='cb-ovr-flo cb-text-preview')
+        # toss = match_name.find('div', class_='cb-ovr-flo cb-text-preview')
+        score = match_name.find(
+            'div', class_='cb-ovr-flo', style='display:inline-block; width:140px')
+        today = dt.now()
+        # today = today.strftime("%d/%m/%Y %H:%M:%S")
+        # today = today.strftime("%d/%m/%Y")
         name = match_name['title']
-        resu = result.text
+        name = name.split("-", 1)
+        name = name[0]
 
-        notification.notify(
-            # title of the notification,
-            title="Match on {}".format(datetime.date.today()),
-            # the body of the notification
-            message=name + '\n' + resu,
+        # print(score)
 
-            app_icon="cricket.ico",
-            # the notification stays for 50sec
-            timeout=10
-        )
+        if result and score is None:
 
-    # while(True):
+            resu = result.text
 
-    #     #sleep for 4 hrs => 60*60*4 sec
-    #     #notification repeats after every 4hrs
-    #     time.sleep(60*60*4)
+            # print(name)
+            # print(resu)
+
+            if resu:
+                toast.show_toast("Today's match:", name + '\n' +
+                                 resu, duration=15, icon_path=resource_path("bell.ico"))
+
+
+def checkNet(i):
+    # if i == 0:
+    # toast.show_toast("CricPing", "Checking for internet connection...",
+    #                  duration=15, icon_path=resource_path("alert.ico"))
+    try:
+        response = requests.get("http://www.google.com")
+        global j
+        if j == False:
+            # toast.show_toast("CricPing", "Connected to the Internet.",
+            #                  duration=15, icon_path=resource_path("alert.ico"))
+            j = True
+        return True
+    except requests.ConnectionError:
+        if i % 7 == 0:
+            toast.show_toast("CricPing", "Check Your Internet connection!",
+                             duration=15, icon_path=resource_path("alert.ico"))
+        return False
+
+
+i = 0
+j = False
+
+# checkNet(i)
+# cric()
+
+while(True):
+
+    checkNet(i)
+    cric()
+    # sleep for 4 hrs => 60*60*4 sec
+    time.sleep(60*30)
